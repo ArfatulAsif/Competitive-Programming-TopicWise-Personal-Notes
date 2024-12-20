cdf2f5f1c5a0ffb4aa68b25069b0c26b52965f9d
Arfatul Islam Asif <107148686+ArfatulAsif@users.noreply.github.com>
2024-12-21 05:49:06 +0600

    Update Maximum Bipartite Maching.cpp

--- FLOW/Maximum Bipartite Maching.cpp
+++ FLOW/Maximum Bipartite Maching.cpp
@@ -1,1 +1,7 @@
-// Maching: In a Graph G, Maching is the task of selecting a set of edges, such that no two edges have same end points(vertices).
+// Matching: In a Graph G, Matching is the task of selecting a set of edges, such that no two edges have same end points(vertices).
+
+// Bipartite Matching: Selecting a set of edges in a bipartite graph, such that no two edges have same end points.
+// Maximum Bipartite Matching = The maximum possible number of edges that can exists in a bipartite matching.
+
+// This can be solved using max flow theorems. 
+
