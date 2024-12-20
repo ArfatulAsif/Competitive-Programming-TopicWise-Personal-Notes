d2db42788fc9b49ecd3988f7c93c9fa08542e982
Arfatul Islam Asif <107148686+ArfatulAsif@users.noreply.github.com>
2024-12-21 04:10:12 +0600

    Update Max Flow (for general use) (Dinic's algorithm) O(V^2 * E).cpp

--- FLOW/Max Flow (for general use) (Dinic's algorithm) O(V^2 * E).cpp
+++ FLOW/Max Flow (for general use) (Dinic's algorithm) O(V^2 * E).cpp
@@ -1,1 +1,5 @@
+// Use this code for finding max flow
+// The time complexity is O(V^2 * E). Which is most practicall, as normally number of vertices V are less than edges.
 
+
+// Level Graphs and Blocking Flows: By focusing only on augmenting paths in the current level graph, Dinic minimizes redundant or unnecessary path exploration. This reduces the total number of augmentations compared to Edmonds-Karp, which may repeatedly explore longer paths.
