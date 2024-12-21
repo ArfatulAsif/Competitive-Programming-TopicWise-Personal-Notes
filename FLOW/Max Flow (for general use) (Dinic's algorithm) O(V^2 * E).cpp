75f2c7a0e7df78fd936f72cc5d70955cc6d2d924
Arfatul Islam Asif <107148686+ArfatulAsif@users.noreply.github.com>
2024-12-21 06:10:25 +0600

    Update Max Flow (for general use) (Dinic's algorithm) O(V^2 * E).cpp

--- FLOW/Max Flow (for general use) (Dinic's algorithm) O(V^2 * E).cpp
+++ FLOW/Max Flow (for general use) (Dinic's algorithm) O(V^2 * E).cpp
@@ -1,6 +1,5 @@
 // Use this code for finding max flow
 // The time complexity is O(V^2 * E). Which is most practicall, as normally number of vertices V are less than edges.
-// For a unit capacity flow network its complexity is O(√V * E). This is very important to remember. ICPC and IUPC gives problem of these. addEdge(u,v,1) for unit capacity flow network
 
 // Using a BFS to build a level graph, then iterate over it multiple times using DFS. and repeat it (one BFS, multiple DFS).
 // Level Graphs and Blocking Flows: By focusing only on augmenting paths in the current level graph , Dinic minimizes redundant or unnecessary path exploration. This reduces the total number of augmentations compared to Edmonds-Karp, which may repeatedly explore longer paths.
